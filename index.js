962df9e880f3dc88e8331550b5f86348218ffc1a
mrlaboratory <mrlaboratory23@gmail.com>
2024-01-09 02:40:54 +0600

    claster updated

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rkuxfhq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jpkply.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, {
