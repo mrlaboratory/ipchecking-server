9e1bfefa27411b23baeb2bcae92a5d6f6970eeed
mrlaboratory <mrlaboratory23@gmail.com>
2024-01-09 15:10:06 +0600

    dynamic claster

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jpkply.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_Claster}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 (async () => { await client.connect(); })()
 const database = client.db('data');
